38c0e7df210deb6b44a9278748f4b64511b3daba
EC2 Default User <ec2-user@ip-172-31-20-106.ec2.internal>
2022-02-28 11:04:01 +0000

    [Add]いいね機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -15,4 +16,7 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+ 
+  
 end
