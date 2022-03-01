36e6f5feecceb7e27159bd4896509db367d3a61d
EC2 Default User <ec2-user@ip-172-31-20-106.ec2.internal>
2022-03-01 09:17:33 +0000

    [Add]コメント機能途中

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about",as:"about"
   resources :users, only: [:index,:show,:edit,:update]
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create,:destroy]
     resource :favorites,only: [:create, :destroy]
   end
   
